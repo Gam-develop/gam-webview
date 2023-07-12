36af1a36040285a372bab7a06e6089d228016388
dragontaek-lee <dragontaek98@naver.com>
2023-07-13 05:28:19 +0900

    feat: page layout

---
+++ src/components/PageLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PageLayout = (props) => {
+  const { children } = props;
+  return <St.PageWrapper>{children}</St.PageWrapper>;
+};
+
+export default PageLayout;
+
+const St = {
+  PageWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: center;
+    align-items: center;
+
+    width: 100%;
+    height: 100%;
+  `,
+};
