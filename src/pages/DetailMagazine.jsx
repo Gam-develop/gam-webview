9eab292c174ff6c456730fa06df6ca1dab23e5e0
dragontaek-lee <dragontaek98@naver.com>
2023-07-13 05:27:32 +0900

    feat: detail page

---
+++ src/pages/DetailMagazine.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+import PageLayout from '../components/PageLayout';
+import Magazine from '../components/Magazine';
+
+const DetailMagazine = () => {
+  return (
+    <PageLayout>
+      <Magazine />
+      <Outlet />
+    </PageLayout>
+  );
+};
+
+export default DetailMagazine;
