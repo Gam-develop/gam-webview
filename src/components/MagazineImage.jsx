8711176a4e62a14ffb05ab4a21dae8548c14e9fb
dragontaek-lee <dragontaek98@naver.com>
2023-07-13 20:04:59 +0900

    feat: MagazineImages component

---
+++ src/components/MagazineImage.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const MagazineImage = (props) => {
+  const { magazinePhotos } = props;
+
+  const settings = {
+    dots: true,
+    arrows: false,
+  };
+
+  return (
+    <St.MagazineImages>
+      <Slider {...settings}>
+        {magazinePhotos.map((url) => {
+          return <St.MagazineImage src={url} alt="app slider" height={300} />;
+        })}
+      </Slider>
+    </St.MagazineImages>
+  );
+};
+
+export default MagazineImage;
+
+const St = {
+  MagazineImages: styled.div`
+    width: 100%;
+
+    & > .slick-slider > .slick-dots > li {
+      width: 0.7rem;
+      height: 0.7rem;
+      background-color: ${({ theme }) => theme.colors.Gam_Gray};
+      border-radius: 0.5rem;
+
+      & > button {
+        display: none;
+      }
+    }
+
+    & > .slick-slider > .slick-dots > .slick-active {
+      width: 2.1rem;
+      height: 0.7rem;
+      background-color: ${({ theme }) => theme.colors.Gam_Yellow};
+      border-radius: 0.5rem;
+      border: 0.1rem solid ${({ theme }) => theme.colors.Gam_Gray};
+    }
+  `,
+
+  MagazineImage: styled.img`
+    width: 100%;
+    height: 364px;
+  `,
+};
