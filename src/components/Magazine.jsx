77d1987a31d680c87256e1d62d6c6d82e9e61b6a
dragontaek-lee <dragontaek98@naver.com>
2023-07-13 20:45:15 +0900

    refactor: replace to question component

--- src/components/Magazine.jsx
+++ src/components/Magazine.jsx
@@ -1,19 +1,15 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import theme from '../styles/theme';
 import styled from 'styled-components';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import useGetMagazineDetail from '../lib/hooks/useGetMagazineDetail';
 import { magazineDetailState } from '../recoil/atom';
 import ErrorPage from '../pages/ErrorPage';
-import { useCollapse } from 'react-collapsed';
-import { ReactComponent as IcCollapse } from '../assets/icon/IcCollapse.svg';
-import { ReactComponent as IcExpand } from '../assets/icon/IcExpand.svg';
 import MagazineImage from './MagazineImage';
+import MagazineQuestion from './MagazineQuestion';
 
 const Magazine = () => {
   const { magazineId } = useParams();
-  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
   const { magazineResult, isLoading, isError } = useGetMagazineDetail(magazineId);
 
   const setMagazineDetail = useSetRecoilState(magazineDetailState);
@@ -21,7 +17,6 @@ const Magazine = () => {
 
   useEffect(() => {
     if (magazineResult) {
-      console.log(magazineResult.data.magazineIntro);
       setMagazineDetail(magazineResult.data);
     }
   }, [magazineResult]);
@@ -33,31 +28,7 @@ const Magazine = () => {
     <St.MagazineWrapper>
       <MagazineImage magazinePhotos={magazineDetail.magazinePhotos}></MagazineImage>
       <St.MagazineIntro>{magazineDetail.magazineIntro}</St.MagazineIntro>
-      <St.MagazineQuestions>
-        {magazineDetail.questions.map((question) => {
-          return (
-            // component로 분리, with key questionId
-            <div>
-              <div style={{ display: 'flex' }}>
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} key={question.questionId}>
-                  <St.MagazineQuestion> {`Q${question.questionOrder}`}</St.MagazineQuestion>
-                </div>
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} key={question.questionId}>
-                  <St.MagazineQuestion> {question.question}</St.MagazineQuestion>
-                </div>
-                <div className="header" {...getToggleProps()} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                  {isExpanded ? <IcCollapse /> : <IcExpand />}
-                </div>
-              </div>
-              <div {...getCollapseProps()}>
-                <St.MagazineIntro key={question.questionId}>{question.answer}</St.MagazineIntro>
-                <St.MagazineIntro key={question.questionId}>{question.answerImage}</St.MagazineIntro>
-                <St.MagazineIntro key={question.questionId}>{question.imageCaption}</St.MagazineIntro>
-              </div>
-            </div>
-          );
-        })}
-      </St.MagazineQuestions>
+      <MagazineQuestion magazineQuestions={magazineDetail.questions}></MagazineQuestion>
     </St.MagazineWrapper>
   );
 };
@@ -82,18 +53,6 @@ const St = {
     margin: 4.168rem 0rem 1.3rem 0rem;
     font-size: 1.5rem;
     white-space: pre-wrap;
-    color: ${({ theme }) => theme.colors.Gam_Black};
-    ${({ theme }) => theme.fonts.Gam_Contend_Pretendard_Regular};
-  `,
-
-  MagazineQuestion: styled.div`
-    white-space: pre-wrap;
-    color: ${({ theme }) => theme.colors.Gam_Black};
-    ${({ theme }) => theme.fonts.Gam_Contend_Pretendard_Medium};
-  `,
-
-  MagazineQuestions: styled.div`
-    width: 100%;
     color: ${({ theme }) => theme.colors.Gam_Black};
     ${({ theme }) => theme.fonts.Gam_Contend_Pretendard_Regular};
   `,
