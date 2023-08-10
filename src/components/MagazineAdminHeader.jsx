502b0556e950f1f3a18f59d8a2bb736871201ebf
dragontaek-lee <dragontaek98@naver.com>
2023-08-11 01:21:04 +0900

    feat: header for admin

---
+++ src/components/MagazineAdminHeader.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { ReactComponent as IcGam } from '../assets/icon/IcGam.svg';
+
+const MagazineAdminHeader = () => {
+  return (
+    <StHeaderWrapper>
+      <IcGam />
+    </StHeaderWrapper>
+  );
+};
+
+export default MagazineAdminHeader;
+
+const StHeaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.Gam_Black};
+
+  padding: 20px 0px;
+
+  width: 100%;
+  height: 120px;
+`;
