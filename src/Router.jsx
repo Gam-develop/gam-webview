ca2de1f3a6eef2ffcdba77007fdf2bf4ac0050e0
dragontaek-lee <dragontaek98@naver.com>
2023-08-11 01:17:02 +0900

    feat: add magazine list, create for admin

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DetailMagazine from './pages/DetailMagazine';
 import CreateMagazine from './pages/CreateMagazine';
+import MainList from './pages/MainList';
 import { RecoilRoot } from 'recoil';
 
 const Router = () => {
@@ -9,7 +9,8 @@ const Router = () => {
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path="/" element={<CreateMagazine />} />
+          <Route path="/magazine/list" element={<MainList />} />
+          <Route path="/magazine/create" element={<CreateMagazine />} />
           <Route path="/magazine/:magazineId" element={<DetailMagazine />} />
         </Routes>
       </RecoilRoot>
