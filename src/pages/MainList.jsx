283bfa395b4dd42febb1c1d4fd3f70c281a96bc9
dragontaek-lee <dragontaek98@naver.com>
2023-08-11 01:19:20 +0900

    feat: main list page

---
+++ src/pages/MainList.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+import PageLayout from '../components/PageLayout';
+import MagazineList from '../components/MagainzeList';
+import MagazineAdminHeader from '../components/MagazineAdminHeader';
+
+const MainList = () => {
+  return (
+    <PageLayout>
+      <MagazineAdminHeader />
+      <MagazineList />
+      <Outlet />
+    </PageLayout>
+  );
+};
+
+export default MainList;
