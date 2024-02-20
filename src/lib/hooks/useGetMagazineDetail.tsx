54e4a9627a902a10fdc3e23ccfb2808c4084ee14
boeunLee <bounjaeun@gmail.com>
2024-02-20 17:20:12 +0900

    [REFACTOR] magazineId 없을때 체크

--- src/lib/hooks/useGetMagazineDetail.tsx
+++ src/lib/hooks/useGetMagazineDetail.tsx
@@ -5,6 +5,14 @@ import { gamGetFetcher } from '../axios';
 import { magazineDetail } from '../../types/magazine';
 
 const useGetMagazineDetail = (magazineId: string) => {
+  // magazineId가 없을때 체크
+  if (!magazineId) {
+    return {
+      magazineDetailResult: null,
+      isLoading: false,
+      isError: false,
+    };
+  }
   const { data, error } = useSWR<AxiosResponse<magazineDetail>>(`/api/v1/admin/magazine/detail?magazineId=${+magazineId}`, gamGetFetcher, {
     errorRetryCount: 3,
     shouldRetryOnError: false,
