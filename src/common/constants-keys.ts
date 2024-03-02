a3d7e0430017dcc5b0112f079f79a0efa9fb6469
boeunLee <bounjaeun@gmail.com>
2024-03-02 22:51:37 +0900

    [FEAT] env 추가
    프론트엔드 호스트 주소, redirect url, 앱 키(JavaScript key)를 추가합니다.

---
+++ src/common/constants-keys.ts
@@ -0,0 +1,4 @@
+const KEYS = {
+  KEY_JS: import.meta.env.VITE_JS_KEY,
+};
+export { KEYS };
--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,9 @@ interface ImportMeta {
     VITE_API_URI: string;
     VITE_TEMP_TOKEN: string;
     VITE_IMAGE_URL: string;
+    VITE_DEV_URL: string;
+    VITE_JS_KEY: string;
+    VITE_REDIRECT_URL: string;
   };
 }
 
