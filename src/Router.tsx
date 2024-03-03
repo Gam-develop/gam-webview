426e824f85bcff4669788eb841cb7f509f1a11e2
boeunLee <bounjaeun@gmail.com>
2024-03-03 23:50:48 +0900

    [FEAT] url 직접 접근 제한

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DetailMagazine from './pages/DetailMagazine';
 import CreateMagazine from './pages/CreateMagazine';
 import MainList from './pages/MainList';
 import { RecoilRoot } from 'recoil';
-import CreateMagazineDemo from './pages/CreateMagazine';
-import Magazine from './components/Magazine';
 import Login from './pages/Login';
+import PublicRoute from './pages/PublicRoute';
+import PrivateRoute from './pages/PrivateRoute';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <RecoilRoot>
         <Routes>
-          <Route path="/auth">
-            <Route path="/auth" element={<Login />} />
-            <Route path="/auth/kakao/callback" element={<Login />} />
+          <Route element={<PublicRoute />}>
+            <Route path="/auth">
+              <Route path="/auth" element={<Login />} />
+              <Route path="/auth/kakao/callback" element={<Login />} />
+            </Route>
           </Route>
-          <Route path="/" element={<MainList />} />
-          <Route path="/magazine/create" element={<CreateMagazine />} />
-          <Route path="/magazine/create/:magazineId" element={<CreateMagazine />} />
-          <Route path="/magazine/:magazineId" element={<DetailMagazine />} />
+
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<MainList />} />
+            <Route path="/magazine/create" element={<CreateMagazine />} />
+            <Route path="/magazine/create/:magazineId" element={<CreateMagazine />} />
+            <Route path="/magazine/:magazineId" element={<DetailMagazine />} />
+          </Route>
         </Routes>
       </RecoilRoot>
     </BrowserRouter>
