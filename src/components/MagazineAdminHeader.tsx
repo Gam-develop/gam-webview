dcd9f3e02576e48d40b60045a6d414174f93c94d
boeunLee <bounjaeun@gmail.com>
2024-03-03 22:03:12 +0900

    [FEAT] 카카오 로그인, 로그아웃 구현 완료

--- src/components/MagazineAdminHeader.tsx
+++ src/components/MagazineAdminHeader.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ReactComponent as IcGam } from '../assets/icon/IcGam.svg';
+import { Link, useNavigate } from 'react-router-dom';
+import { clearUserSession } from '../lib/token';
 
 const MagazineAdminHeader = (props: any) => {
   const { isMainList } = props;
+  const handleClickLogout = () => {
+    clearUserSession();
+    window.alert('로그아웃 되었습니다.');
+  };
   return (
     <StHeaderWrapper>
       <IcGam />
-      {isMainList && <StLogOut>로그아웃</StLogOut>}
+      {isMainList && (
+        <StLogOut to="/auth" onClick={handleClickLogout}>
+          로그아웃
+        </StLogOut>
+      )}
     </StHeaderWrapper>
   );
 };
@@ -29,11 +39,12 @@ const StHeaderWrapper = styled.div`
   }
 `;
 
-const StLogOut = styled.p`
+const StLogOut = styled(Link)`
   position: absolute;
   width: 12.2rem;
   padding: 1.2rem 1.6rem;
   right: 7.6rem;
+  text-decoration: none;
   color: ${({ theme }) => theme.colors.Gam_White};
   ${({ theme }) => theme.fonts.Gam_Contend_Pretendard_Bold_26};
 `;
--- src/lib/api/login.ts
+++ src/lib/api/login.ts
@@ -1,13 +1,12 @@
-import { useNavigate } from 'react-router-dom';
 import { client } from '../axios';
-import { setAccessToken } from '../token';
+import { setUserSession } from '../token';
 import { LoginDto } from './dto/login.dto';
 
 export const adminLogin = async (param: LoginDto) => {
   try {
     const { data } = await client.post('/api/v1/social/login', param);
     if (data.data) {
-      setAccessToken('accessToken', data.data.accessToken);
+      setUserSession(data.data.accessToken);
     }
     return data;
   } catch (error) {
