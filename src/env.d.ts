b16af9c06bafc6808c11d688eaae197bb1bf544e
boeunLee <bounjaeun@gmail.com>
2024-03-10 23:01:18 +0900

    [STYLE] svg 파일을 컴포넌트로 변환시키는 코드를 env파일에서 제거

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,10 +8,3 @@ interface ImportMeta {
     VITE_REDIRECT_URL: string;
   };
 }
-
-declare module '*.svg' {
-  import React = require('react');
-  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
